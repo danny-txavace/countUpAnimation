bf7479c6f38674724c993697b205087d303ffda1
RamadanismaeL <ramadan.ismael02@gmail.com>
2025-09-11 08:39:15 +0200

    updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component, ElementRef, OnInit, signal, ViewChild } from '@angular/core';
 import { CountUp } from 'countup.js';
 import { CountUpModule } from 'ngx-countup';
+import { CountUpss } from "./count-ups/count-upss";
+import { Home } from "./home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [CountUpModule],
+  imports: [CountUpModule, CountUpss, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
@@ -14,7 +16,25 @@ export class App implements AfterViewInit, OnInit {
   @ViewChild('countUpElement') countUpElement!: ElementRef;
 
   ngAfterViewInit() {
-    const countUp = new CountUp(this.countUpElement.nativeElement, 20021.75, {
+    /*const countUp = new CountUp(this.countUpElement.nativeElement, 20021.75, {
+      duration: 2,
+      separator: ' ',
+      decimal: ',',
+      decimalPlaces: 2
+    });
+    if (!countUp.error) {
+      countUp.start();
+    }*/
+
+      this.onCountUp(this.countUpElement, 2345);
+  }
+
+  ngOnInit(): void {
+    //this.onCountUp();
+  }
+
+  onCountUp(element: ElementRef, value: number) {
+    const countUp = new CountUp(element.nativeElement, value, {
       duration: 2,
       separator: ' ',
       decimal: ',',
@@ -23,9 +43,5 @@ export class App implements AfterViewInit, OnInit {
     if (!countUp.error) {
       countUp.start();
     }
-  }
-
-  ngOnInit(): void {
-    //this.onCountUp();
   }
 }
---
+++ src/app/home/home.ts
@@ -0,0 +1,70 @@
+import { Component, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { BehaviorSubject, combineLatest, switchMap, animationFrameScheduler, interval, map, takeWhile, endWith, distinctUntilChanged } from 'rxjs';
+import { CountUpModule } from "ngx-countup";
+
+const easeOutQuad = (x: number): number => x * (2 - x);
+
+@Component({
+  selector: 'app-home',
+  imports: [CountUpModule],
+  templateUrl: './home.html',
+  styleUrl: './home.scss'
+})
+export class Home implements OnInit {
+  targetValue = 100;
+
+  private readonly count$ = new BehaviorSubject(0);
+  private readonly duration$ = new BehaviorSubject(2000);
+
+  private readonly currentCount$ =
+  combineLatest([
+    this.count$,
+    this.duration$
+  ]).pipe(
+    switchMap(([count, duration]) => {
+      const startTime = animationFrameScheduler.now();
+
+      return interval(0,
+        animationFrameScheduler).pipe(
+          map(() => animationFrameScheduler.now() - startTime),
+          map((elapsedTime) => elapsedTime / duration),
+          takeWhile((progress) => progress <= 1),
+          map(easeOutQuad),
+          map((progress) => Math.round(progress * count)),
+          endWith(count),
+          distinctUntilChanged()
+        );
+    })
+  );
+
+  @Input('countUps')
+  set count(count: number) {
+    this.count$.next(count);
+  }
+
+  @Input()
+  set duration(duration: number) {
+    this.duration$.next(duration);
+  }
+
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2
+  )
+  {}
+
+  ngOnInit(): void {
+    this.displayCurrentCount();
+  }
+
+  private displayCurrentCount(): void {
+    this.currentCount$
+      .subscribe((currentCount) => {
+        this.renderer.setProperty(
+          this.elementRef.nativeElement,
+          'innerHTML',
+          currentCount
+        );
+      });
+  }
+}
